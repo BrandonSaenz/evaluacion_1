45a4d1a2a8686b978a7a5b7de1788ef69bb0142c
BrandonSaenz <bsz.calistenia@gmail.com>
2021-11-12 00:14:17 -0800

    Intento de custom iframe

--- src/JS/script.js
+++ src/JS/script.js
@@ -50,14 +50,14 @@ function goContent(index){
 
 function crearMenu(){
     let ul_menu=document.getElementById('ul_menu');
-    let li='';
+    var li='';
     
     for(var i=0; i<=list_menu.length-1; i++){
         li+=`<li onclick="goContent(${i})"><a href="#">${list_menu[i].title}</a></li>`;
-        if(i==list_menu.length-1){
-            ul_menu.innerHTML=li;
-        }
+     
     }
+
+    ul_menu.innerHTML=li;
 }
 
 function crearSecciones(){
@@ -66,9 +66,20 @@ function crearSecciones(){
         let div=document.createElement('div');
         div.classList='section'; div.id=`section_${i}`;
         div.style.left=`${(100*i)}%`;
-       
+
+        const frame=document.createElement('iframe');
+        frame.id='frame-suma'; frame.src='src/pages/suma.html';
+                     
+
+        div.appendChild(frame);
+        content.appendChild(div);         
+
+    }
 }
 
+function action(){
+    console.log('FUNCION DE IFRAME');
+}
 
 var go={
     suma: function(){
@@ -80,6 +91,7 @@ var go={
 }
 
 window.onload = function() {
+    // window.onerror = function(){return true;}
     crearMenu();
     crearSecciones();
 };
