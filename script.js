0c4fb0753c9b9cf5b68b243344f8d20e1c1582d0
BrandonSaenz <51378587+BrandonSaenz@users.noreply.github.com>
2021-11-11 16:18:19 -0800

    Alta de archivos del proyecto
    Actualización rapida

---
+++ script.js
@@ -0,0 +1,93 @@
+var list_menu=[
+    {
+        id: '1',
+        title:'suma',
+        option_code:'suma'
+        
+    },
+    {
+        id: '2',
+        title:'resta',
+        option_code:'resta'
+        
+    },
+    {
+        id: '3',
+        title:'multiplicación',
+        option_code:'multi'
+        
+    },
+    {
+        id: '4',
+        title:'división',
+        option_code:'division'
+        
+    },
+    {
+        id: '5',
+        title:'raíz cuadrada',
+        option_code:'raiz'
+        
+    },
+    {
+        id: '6',
+        title:'fórmula general',
+        option_code:'form_gen'
+        
+    },
+    {
+        id: '7',
+        title:'binomio cuadrado perfecto',
+        option_code:'bin_cp'
+        
+    }
+];
+
+function goContent(index){
+    let content=document.getElementById('content');
+    content.scrollLeft=content.clientWidth*index;
+}
+
+function crearMenu(){
+    let ul_menu=document.getElementById('ul_menu');
+    let li='';
+    
+    for(var i=0; i<=list_menu.length-1; i++){
+        li+=`<li onclick="goContent(${i})"><a href="#">${list_menu[i].title}</a></li>`;
+        if(i==list_menu.length-1){
+            ul_menu.innerHTML=li;
+        }
+    }
+}
+
+function crearSecciones(){
+    let content=document.getElementById('content');
+    for(var i=0; i<=list_menu.length-1; i++){
+        let div=document.createElement('div');
+        div.classList='section'; div.id=`section_${i}`;
+        div.style.left=`${(100*i)}%`;
+
+        let title=document.createElement('p');
+        title.classList='mayus text-center title-content';
+        title.textContent=list_menu[i].title;
+
+        let useImportPage=document.getElementById('sumaImport');
+
+        div.innerHTML=useImportPage;
+        content.appendChild(div);
+    }
+}
+
+var go={
+    suma: function(){
+       
+    },
+    resta: function(){
+
+    }
+}
+
+window.onload = function() {
+    crearMenu();
+    crearSecciones();
+};
